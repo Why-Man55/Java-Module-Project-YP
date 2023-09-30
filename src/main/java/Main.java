4803ce406dcda3e89c46b1b4346670d2284d2ee7
Why-Man55 <Why2Man4@gmail.com>
2023-09-30 16:10:37 +0300

    Проект 1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,67 @@
 
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+        System.out.println("На сколько человек хотите разделить покупку?");
+        int n = scan.nextInt();
+        while(true)
+        {
+            if(n == 1)
+            {
+                System.out.println("Ощибка! Введите значение >1!");
+            }
+            else if(n < 1)
+            {
+                System.out.println("Ощибка! Некорректное число! Введите значение >1!");
+            }
+            else
+            {
+                double allPrice = 0;
+                String allNames = "";
+
+                while(true)
+                {
+                    System.out.println("Введите название товара или Завершить!");
+                    String name = scan.next();
+                    if(name.equalsIgnoreCase("Завершить"))
+                    {
+                        System.out.println("Ваши товары: " + allNames);
+                        Res(allPrice / n);
+                        break;
+                    }
+                    else
+                    {
+                        allNames = allNames + name + " ";
+                        System.out.println("Введите цену товара!");
+                        double price = scan.nextDouble();
+                        allPrice += price;
+                    }
+                }
+                break;
+            }
+        }
+
+    }
+
+    public static void Res(double price)
+    {
+        String res = "Цена для каждого: %.2f %s";
+        if(Math.floor(price) == 11)
+        {
+            System.out.println(String.format(res, price, "рублей"));
+        }
+        else if(Math.floor(price) % 10 == 1)
+        {
+            System.out.println(String.format(res, price, "рубль"));
+        }
+        else if(Math.floor(price) % 10 > 1 && Math.floor(price) % 10 < 5)
+        {
+            System.out.println(String.format(res, price, "рубля"));
+        }
+        else
+        {
+            System.out.println(String.format(res, price, "рублей"));
+        }
     }
 }
